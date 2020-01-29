860890a3933f3f2ad7a66c15fb051db2caec0444
Vaidas <shibalba1@gmail.com>
2020-01-29 21:14:16 +0100

    Updates

---
+++ precache-manifest.1d498a41346e5d33f439fa4279f5e832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40700638487f00025fed391843fd490",
+    "url": "/handyproxy/handyproxy/index.html"
+  },
+  {
+    "revision": "e06330cb96eb937d996e",
+    "url": "/handyproxy/handyproxy/static/css/2.46fbdb1c.chunk.css"
+  },
+  {
+    "revision": "cabe112d19be1222803f",
+    "url": "/handyproxy/handyproxy/static/css/main.a5df7661.chunk.css"
+  },
+  {
+    "revision": "e06330cb96eb937d996e",
+    "url": "/handyproxy/handyproxy/static/js/2.03c04aab.chunk.js"
+  },
+  {
+    "revision": "cabe112d19be1222803f",
+    "url": "/handyproxy/handyproxy/static/js/main.bf0eaa1d.chunk.js"
+  },
+  {
+    "revision": "1c8096ea0a686f172b2b",
+    "url": "/handyproxy/handyproxy/static/js/runtime-main.82d33c73.js"
+  }
+]);
